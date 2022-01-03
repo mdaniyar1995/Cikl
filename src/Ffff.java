5bf34117e411b8ace8b093f9fb8469aa296961a1
Daniyar <mdaniyar1995@gmail.com>
2022-01-03 23:06:34 +0300

    first commit

---
+++ src/Ffff.java
@@ -0,0 +1,14 @@
+public class Ffff {
+
+    public static void main(String[] args) {
+        int i = 45;
+        while (i < 350) {
+            i++;
+        }
+
+        System.out.println("Value i = " + i);
+    }
+}
+
+
+
