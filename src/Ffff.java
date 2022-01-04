ad4c736eb1552fe3874bdc5fd614072c8e24651c
Daniyar <mdaniyar1995@gmail.com>
2022-01-04 17:02:24 +0300

    svb

--- src/Ffff.java
+++ src/Ffff.java
@@ -1,12 +1,7 @@
 public class Ffff {
 
     public static void main(String[] args) {
-        int i = 45;
-        while (i < 350) {
-            i++;
-        }
-
-        System.out.println("Vfnfn " + i);
+        System.out.println("Vfnfn ");
     }
 }
 
